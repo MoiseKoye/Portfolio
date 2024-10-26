9a74b3592b36017321c9de4d768fcd012d088b0e
MoiseKoye <moisekoye15@gmail.com>
2024-10-26 15:05:33 +0000

    push_portfolio

---
+++ app.js
@@ -0,0 +1,25 @@
+/* alert("C'est mon premier code en JavaScript");
+alert('Merci pour votre attention ');
+window.alert("Merci"); */
+/*
+ var heure = 12;
+ 
+ if(heure < 10){
+    alert("Matin");
+ }else if (heure == 12){
+    alert("midi");
+ } else if(heure>18){
+    alert("soir");
+ }
+*/
+ /*
+var resultat= addition(10,10);
+ function addition(x,y) {
+   alert(x+y);
+   return x+y;
+   
+ }
+
+   addition(20,20);
+*/
+
